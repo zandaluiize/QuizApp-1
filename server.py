2fc9f0f57f763633832543aee086c3e73e6cfb0a
llamaoralpaqa <101086296+llamaoralpaqa@users.noreply.github.com>
2022-04-04 08:50:13 +0300

    TT

--- server.py
+++ server.py
@@ -4,6 +4,22 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures = []
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt - 1
+    while line
+    if line:
+      fullPicInfo = line.strip().split(';')
+      pictures.apped(fullPicInfo[1])
+    line = fp.readline()
+    cnt += 1
+  fp.close()
+  return pictures
+
+
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
@@ -18,6 +34,7 @@ def health():
 def test():
   parametri = ["IQ","Augums","Kājas izmērs"]
   images = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fsnl.no%2FGoogle&psig=AOvVaw28z_X5IU8r-6BwErqgXGvX&ust=1648534514844000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKixg82T6PYCFQAAAAAdAAAAABAY","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fbusiness-55660682&psig=AOvVaw28z_X5IU8r-6BwErqgXGvX&ust=1648534514844000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKixg82T6PYCFQAAAAAdAAAAABAd","https://www.google.com/url?sa=i&url=https%3A%2F%2Ftechcrunch.com%2F2020%2F10%2F06%2Fgoogles-new-logos-are-bad%2F&psig=AOvVaw28z_X5IU8r-6BwErqgXGvX&ust=1648534514844000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCKixg82T6PYCFQAAAAAdAAAAABAj"]
+  images = loadAllPictures()
   return render_template("test.html")
 
 @app.route('/about')
